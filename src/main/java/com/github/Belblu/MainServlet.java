2b9b923890c22642132cfe9ac705706891f36086
Belbli <andey.dyokin@gmail.com>
2020-03-23 01:23:41 +0300

    co

--- src/main/java/com/github/Belblu/MainServlet.java
+++ src/main/java/com/github/Belblu/MainServlet.java
@@ -1,25 +1,26 @@
 package com.github.Belblu;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class MainServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/page.jsp").forward(request, response);
+    }
 
+        @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
     }
 }
---
+++ src/main/webapp/LoginSuccess.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+<title>Login Success Page</title>
+</head>
+<body>
+<h3>Hi User, Login successful.</h3>
+<a href="index.html">Login Page</a>
+</body>
+</html>
